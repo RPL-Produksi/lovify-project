81737b0852b122e47f60fff214d456dd5f1d9dde
Ello17 <azzellofabian@gmail.com>
2025-01-15 13:11:48 +0700

    penambahan about section

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" scro>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://naramizaru.github.io/awesome-2.0/css/all.css">
     @vite('resources/css/app.css')
-    <title>Lovify |  @yield('title')</title>
+    <title>Lovify | @yield('title')</title>
 </head>
 <body class=" bg-black">
     @yield('content')
 </body>
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
+
 </html>

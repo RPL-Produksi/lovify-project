3829551b55c143ad74556bfbecf48bc5e64079ea
KnowRise <rifaasiraajuddin.123@gmail.com>
2025-02-05 14:43:05 +0700

    Auth

--- app/Http/Controllers/BackEnd/v1/AuthController.php
+++ app/Http/Controllers/BackEnd/v1/AuthController.php
@@ -4,8 +4,25 @@ namespace App\Http\Controllers\Backend\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'login' => 'required',
+            'password' => 'required',
+        ]);
+
+        $loginType = filter_var($request->login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        $admin = $request->has('admin') ? true : false;
+
+        if (Auth::attempt([$loginType => $request->login, 'password' => $request->password])) {
+            $user = Auth::user();
+
+            if ()
+        }
+    }
 }

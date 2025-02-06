5c6171ea05a1faaebcd19cca6c03d3353819bfcb
KnowRise <rifaasiraajuddin.123@gmail.com>
2025-02-06 10:36:35 +0700

    CategoryController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\BackEnd\v1\Admin\AdminCategoryController;
+use App\Http\Controllers\BackEnd\v1\Admins\AdminCategoryController;
 use App\Http\Controllers\BackEnd\v1\Admin\AdminPacketController;
 use App\Http\Controllers\BackEnd\v1\AuthController;
 use App\Http\Controllers\Backend\v1\CategoryController;
@@ -24,10 +24,15 @@ Route::prefix('v1')->group(function () {
         Route::post('/login', 'login');
         Route::post('/logout', 'logout')->middleware('auth:sanctum');
     });
+    // Umum
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::get('/categories/{id?}', [CategoryController::class, 'getCategory']);
+    });
     // Admin
     Route::group(['prefix' => 'admins', 'middleware' => ['can:admin']], function () {
         Route::group(['prefix' => 'categories', 'controller' => AdminCategoryController::class], function () {
             Route::post('/{id?}', 'storeCategory');
+            Route::delete('/{id}', 'deleteCategory');
         });
     });
 });

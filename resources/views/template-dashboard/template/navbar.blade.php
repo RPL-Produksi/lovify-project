e62a5006bc2d8142dfc044bd60b6a6ac09a6de89
KnowRise <rifaasiraajuddin.123@gmail.com>
2025-02-12 16:14:49 +0700

    -

--- resources/views/template-dashboard/template/navbar.blade.php
+++ resources/views/template-dashboard/template/navbar.blade.php
@@ -1,9 +1,10 @@
+<!-- Navbar -->
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa-regular fa-bars"></i>
     </button>
 
-    <h5 class="d-none d-md-block text-rose">Lovify | Weedding</h5>
+    <h5 class="d-none d-md-block text-rose">Lovify | Wedding</h5>
 
     <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown no-arrow">
@@ -19,18 +20,16 @@
                     Profile
                 </a>
                 <div class="dropdown-divider"></div>
-                <form action="{{ route('be.logout') }}" method="POST">
-                    @csrf
-                    <a class="dropdown-item" href="{{ route('be.logout') }}" data-toggle="modal" data-target="#logoutModal">
-                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Keluar
-                    </a>
-                </form>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Keluar
+                </a>
             </div>
         </li>
     </ul>
 </nav>
 
+<!-- Modal Konfirmasi Logout -->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel"
     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
@@ -43,10 +42,10 @@
             </div>
             <div class="modal-body">Pilih "Keluar" di bawah jika Anda siap mengakhiri sesi Anda saat ini.</div>
             <div class="modal-footer">
-                <button class="btn btn-link" type="button" data-dismiss="modal">Batal</button>
-                <form action="" method="POST" class="form-with-loading">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+                <form id="logoutForm" action="{{ route('be.logout') }}" method="POST">
                     @csrf
-                    <button type="submit" class="btn btn-danger btn-loading">
+                    <button type="submit" class="btn btn-danger">
                         <span class="btn-text">Keluar</span>
                         <span class="spinner-border spinner-border-sm d-none" role="status"></span>
                     </button>
